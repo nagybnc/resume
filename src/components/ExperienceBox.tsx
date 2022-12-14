818bb067b1ad0dae883a9e7d4caa12f9abdc5a67
nagybnc <bencet630@gmail.com>
2022-12-14 18:50:42 +0100

    Fix icon align

--- src/components/ExperienceBox.tsx
+++ src/components/ExperienceBox.tsx
@@ -9,7 +9,7 @@ interface ExperienceBoxProps {
 const ExperienceBox = ({ experience }: ExperienceBoxProps) => {
     return (
         <div key={experience.title} className="my-4 border border-[#30363d] p-4 print:my-2 print:p-2">
-            <h3 className="text-lg text-[#58a6ff] print:text-sm">
+            <h3 className="text-lg flex items-center gap-2 text-[#58a6ff] print:text-sm">
                 <BriefcaseIcon className="inline-block h-5 w-5 text-gray-400 print:text-xs" /> {experience.title}
             </h3>
             <p className="text-sm font-bold text-gray-400 print:text-xs">{experience.date}</p>
